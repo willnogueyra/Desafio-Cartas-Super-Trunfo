0a9b95930fdd48e228982d03e57a3437254ba67b
william <will.ribeiro@live.com>
2025-05-24 01:18:25 -0300

    first commit

---
+++ js/deck.js
@@ -0,0 +1,92 @@
+const deck = [
+    {
+      name: "Casca",
+      image: "https://static.wikia.nocookie.net/berserk/images/7/76/Bk1-014.jpg",
+      attributes: {
+        strength: 80,
+        defense: 30,
+        magic: 40,
+      },
+    }, 
+    {
+      name: "Griffith",
+      image: "https://static.wikia.nocookie.net/berserk/images/e/e7/Bk1-009.jpg",
+      attributes: {
+        strength: 70,
+        defense: 50,
+        magic: 20,
+      },
+    },
+    {
+      name: "Guts",
+      image: "https://static.wikia.nocookie.net/berserk/images/0/0a/Bk4-23.jpg",
+      attributes: {
+        strength: 40,
+        defense: 100,
+        magic: 10,
+      },
+    },
+    {
+      name: "Skull Knight",
+      image: "https://static.wikia.nocookie.net/berserk/images/3/3f/Bk4-29.jpg",
+      attributes: {
+        strength: 60,
+        defense: 40,
+        magic: 20,
+      },
+    },
+    {
+      name: "Schierke",
+      image: "https://static.wikia.nocookie.net/berserk/images/6/65/Bk4-25.jpg",
+      attributes: {
+        strength: 60,
+        defense: 40,
+        magic: 20,
+      },
+    },
+    {
+      name: "Puck",
+      image: "https://static.wikia.nocookie.net/berserk/images/5/5f/Bk4-27.jpg/",
+      attributes: {
+        strength: 20,
+        defense: 30,
+        magic: 100,
+      },
+    },
+    {
+      name: "Corkus",
+      image: "https://static.wikia.nocookie.net/berserk/images/a/ab/Bk1-017.jpg",
+      attributes: {
+        strength: 30,
+        defense: 30,
+        magic: 90,
+      },
+    },
+    {
+      name: "Pippin",
+      image: "https://static.wikia.nocookie.net/berserk/images/6/6d/Bk1-019.jpg",
+      attributes: {
+        strength: 70,
+        defense: 60,
+        magic: 10,
+      },
+    },
+    {
+      name: "Judeau",
+      image: "https://static.wikia.nocookie.net/berserk/images/d/dc/Bk1-015.jpg",
+      attributes: {
+        strength: 50,
+        defense: 80,
+        magic: 20,
+      },
+    },
+    {
+      name: "Zodd",
+      image: "https://static.wikia.nocookie.net/berserk/images/5/52/Bk1-132.jpg",
+      attributes: {
+        strength: 60,
+        defense: 40,
+        magic: 60,
+      },
+    },
+]
